33b94e8b046ced039a3a06e9297468fac68799f6
MateusVasc <mvaraujo2004@gmail.com>
2025-03-23 19:55:03 -0300

    feat: adds backend initial structure

---
+++ pomodoro-flow-backend/src/main/java/com/pomodoroflow/pomodoro_flow_backend/PomodoroFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pomodoroflow.pomodoro_flow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroFlowBackendApplication.class, args);
+	}
+
+}
---
+++ pomodoro-flow-backend/src/test/java/com/pomodoroflow/pomodoro_flow_backend/PomodoroFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomodoroflow.pomodoro_flow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
